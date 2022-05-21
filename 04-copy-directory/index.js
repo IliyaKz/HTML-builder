940667cb0a8ea28e9f6becda83d352a2c519edad
IliyaKz <iliya-kuznetsov@mail.ru>
2022-05-21 11:41:46 +0300

    feat: complete 04 task (Sat, May 21, 2022 11:41:46 AM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs/promises');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
+const dirPathCopy = path.join(__dirname, 'files-copy');
 
+async function copyDir(source, copy) {
+  try {
+    await fs.rm(copy, { recursive: true, force: true });
+    await fs.mkdir(copy, {recursive: true});
+    const files = await fs.readdir(source, {withFileTypes: true});
+    files.forEach((file) => {
+      if (file.isFile()) {
+        fs.copyFile(path.join(source, file.name), path.join(copy, file.name));
+        console.log('Файл скопирован');
+      } else if (file.isDirectory()) {
+        copyDir(path.join(source, file.name), path.join(copy, file.name));
+        console.log('Папка скопирована');
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+copyDir(dirPath, dirPathCopy);
