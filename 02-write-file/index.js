51ab3cb59816131b2e8ba4b19f9e1134b788a360
IliyaKz <iliya-kuznetsov@mail.ru>
2022-05-22 12:50:12 +0300

    fix: correct file writing in 2 task (Sun, May 22, 2022 12:50:12 PM)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,15 +5,11 @@ const { stdin: input, stdout: output } = require('process');
 const read = readline.createInterface({ input, output });
 const filePath = path.join(__dirname, 'text.txt');
   
-function create() {
+function write() {
   fs.writeFile(filePath, '', (error) => {
     if (error) return console.error(error.message);
   });
-}
-fs.access(filePath, fs.constants.F_OK, (error) => {
-  if (error) create();
-  read.setPrompt('Добро пожаловать! Начните ввод данных\n');
-  read.prompt();
+  console.log('Добро пожаловать! Начните ввод данных');
   read.on('line', (text) => {
     if (text === 'exit') {
       read.close();
@@ -24,4 +20,5 @@ fs.access(filePath, fs.constants.F_OK, (error) => {
     });
   });
   read.once('close', () => console.log('Ввод данных завершен'));
-});
+}
+write();
